5ee9e177b9de0714539d6920cca0ff32fc67a9c3
arifsari <arif.d.sari@gmail.com>
2021-03-04 21:13:19 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Asalsayı
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            if (IsPrimeNumber(99))
+            {
+                Console.WriteLine(" this is prime");
+            }
+            else
+            {
+                Console.WriteLine(" this is not prime");
+            }
+            Console.ReadLine();
+
+        }
+
+        private static bool IsPrimeNumber(int number)
+        {
+            bool result = true;
+            for (int i = 2; i < number-1; i++)
+            {
+                if (number%i==0)
+                {
+                    result = false;
+                    
+                }
+            }
+            return result;
+        }
+
+    }
+
+}
+
+
+
